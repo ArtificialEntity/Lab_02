ecfba061c46711f62bd355258cec88679c0ee78b
King_In_Yellow <vadim090207@rambler.ru>
2026-09-07 21:09:23 +0300

    Add files via upload

---
+++ Money.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int Passangers = 100;
+
+	int S_Passangers = 3 * Passangers / 4;
+
+	int F_Passangers = 1 * Passangers / 4;
+
+	int S_Price = 50;
+
+	int F_Price = 50 * 2;
+
+	printf("%d - общая выручка", S_Passangers * S_Price + F_Passangers * F_Price);
+
+	return 0;
+}
