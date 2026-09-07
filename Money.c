20ffe0ec27d7bdd88b2291b9de6be8fb4dd82911
King_In_Yellow <vadim090207@rambler.ru>
2026-09-07 21:38:53 +0300

    Money.c

--- Money.c
+++ Money.c
@@ -5,17 +5,17 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 
-	int Passangers = 100;
+	int Passengers = 100;
 
-	int S_Passangers = 3 * Passangers / 4;
+	int S_Passengers = 3 * Passengers / 4;
 
-	int F_Passangers = 1 * Passangers / 4;
+	int F_Passengers = 1 * Passengers / 4;
 
 	int S_Price = 50;
 
 	int F_Price = 50 * 2;
 
-	printf("%d - общая выручка", S_Passangers * S_Price + F_Passangers * F_Price);
+	printf("%d - общая выручка", S_Passengers * S_Price + F_Passengers * F_Price);
 
 	return 0;
 }
